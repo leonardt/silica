32a8190bb8b37951a80b8d8566d2d55263e901a1
Lenny Truong <lenny@cs.stanford.edu>
2017-10-20 22:56:30 -0700

    Auto generate readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Check out these [jupyter notebooks](./notebooks)
+# Examples
+* [rom](./notebooks/rom.ipynb)
+* [shift_register](./notebooks/shift_register.ipynb)
+* [tff](./notebooks/tff.ipynb)
+* [counter](./notebooks/counter.ipynb)
--- generate_notebooks.py
+++ generate_notebooks.py
@@ -6,9 +6,13 @@ for file in ["silica", "counter"]:
     os.remove(f"notebooks/{file}.py")
     os.symlink(f"../{file}.py", f"notebooks/{file}.py")
 
-for file in glob.glob("*.py"):
-    base_name = file.split(".")[0] 
-    if base_name in {"silica", "generate_notebooks"}:
-        continue
-    subprocess.call(f"python -m py2nb {file} notebooks/{base_name}.ipynb".split(" "))
-    subprocess.call(f"jupyter nbconvert --to notebook --inplace --execute notebooks/{base_name}.ipynb".split(" "))
+with open("README.md", "w") as readme:
+    readme.write("# Examples\n")
+    for file in glob.glob("*.py"):
+        base_name = file.split(".")[0] 
+        if base_name in {"silica", "generate_notebooks"}:
+            continue
+        subprocess.call(f"python -m py2nb {file} notebooks/{base_name}.ipynb".split(" "))
+        subprocess.call(f"jupyter nbconvert --to notebook --inplace --execute notebooks/{base_name}.ipynb".split(" "))
+        readme.write(f"* [{base_name}](./notebooks/{base_name}.ipynb)\n")
+
