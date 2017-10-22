ba46a592905496c0fcee4f4434c682a57bd0e113
Lenny Truong <lenny@cs.stanford.edu>
2017-10-21 18:19:38 -0700

    Update lfsr

--- examples/lfsr.py
+++ examples/lfsr.py
@@ -25,23 +25,28 @@ def LFSR(n, taps, init=1):
 
 
 if __name__ == "__main__":
-    lfsr = LFSR(8, taps=[3, 4, 5, 7])
+    width = 8
+    taps = {
+        8: [3, 4, 5, 7],      # Period = 255
+        19: [18, 17, 16, 13]  # Period = 524287
+    }
+    lfsr = LFSR(width, taps=taps[width])
     counter = Counter()
-    print("For a counter of width 8, it should produce 2 * 8 - 1 distinct values before repeating")
-    for i in range(2 ** 8 - 1):
+    print("For a counter of width {width}, it should produce 2 * {width} - 1 distinct values before repeating")
+    for i in range(2 ** width - 1):
         O = seq2int(lfsr.O)
         counter[O] += 1
         print(f"O={O}")
         next(lfsr)
 
     for item, count in counter.items():
-        assert count == 1, "Should only see each binary sequence once every 2 ^ N - 1 cycles"
+        assert count == 1, f"Should only see each binary sequence once every 2 ^ {width} - 1 cycles"
 
-    for i in range(2 ** 8 - 1):
+    for i in range(2 ** width - 1):
         O = seq2int(lfsr.O)
         counter[O] += 1
         print(f"O={O}")
         next(lfsr)
 
     for item, count in counter.items():
-        assert count == 2, "Should only see each binary sequence twice every 2 * (2 ^ N - 1) cycles"
+        assert count == 2, f"Should only see each binary sequence twice every 2 * (2 ^ {width} - 1) cycles"
