a6eb6676b5fdfee0e5c524c49d61f7535573e713
Lenny Truong <lenny@cs.stanford.edu>
2017-11-08 20:32:23 -0800

    Fix lbmem address sharing

--- tests/test_lbmem.py
+++ tests/test_lbmem.py
@@ -16,15 +16,15 @@ def FillingState(lbmem_width, depth, lbmem, raddr, waddr):
     rdata is always invalid
     Remains in this state until `lbmem_width` writes have occured (wen is high for `lbmem` cycles)
     """
-    valid = 0
-    rdata = lbmem[raddr]
-    wdata, wen = yield rdata, valid
     for i in range(lbmem_width):
+        valid = 0
+        rdata = lbmem[raddr]
         wdata, wen = yield rdata, valid
         while not wen:
             wdata, wen = yield rdata, valid
         lbmem[waddr] = wdata
-        waddr = saturating_add(waddr, 1, depth - 1)
+        waddr = saturating_add(waddr, 1, depth)
+    return waddr
 
 @si.generator
 def DrainingState(lbmem_width, depth, lbmem, raddr, waddr):
@@ -33,19 +33,18 @@ def DrainingState(lbmem_width, depth, lbmem, raddr, waddr):
     rdata is always valid
     Remains in this state until `lbmem_width` cycles without a write have occured
     """
-    valid = 1
-    rdata = lbmem[raddr]
-    wdata, wen = yield rdata, valid
     not_drained = lbmem_width
     while not_drained:
+        valid = 1
         rdata = lbmem[raddr]
         wdata, wen = yield rdata, valid
-        raddr = saturating_add(raddr, 1, depth - 1)
+        raddr = saturating_add(raddr, 1, depth)
         if wen:
             lbmem[waddr] = wdata
-            waddr = saturating_add(waddr, 1, depth - 1)
+            waddr = saturating_add(waddr, 1, depth)
         else:
             not_drained -= 1
+    return waddr, raddr
 
 @si.coroutine
 def LbMem(depth=1024,lbmem_width=5):
@@ -53,8 +52,8 @@ def LbMem(depth=1024,lbmem_width=5):
     raddr = uint(0, eval(math.ceil(math.log2(depth))))
     waddr = uint(0, eval(math.ceil(math.log2(depth))))
     while True:
-        yield from FillingState(lbmem_width, depth, lbmem, raddr, waddr)
-        yield from DrainingState(lbmem_width, depth, lbmem, raddr, waddr)
+        waddr = yield from FillingState(lbmem_width, depth, lbmem, raddr, waddr)
+        waddr, raddr = yield from DrainingState(lbmem_width, depth, lbmem, raddr, waddr)
 
 def test_lbmem():
     lbmem = LbMem()
