8fb36439f876a105ae9c7086d15c11ea1ee5ca58
Lenny Truong <lenny@cs.stanford.edu>
2017-11-10 21:28:31 -0800

    Add detect notebook

--- verilog/detect111.v
+++ verilog/detect111.v
@@ -2,7 +2,7 @@ module detect111(
   input clk,
   input I,
   output O
-) 
+);
   reg [1:0] cnt = 0;
 
   wire [1:0] cnt_next;
@@ -11,7 +11,7 @@ module detect111(
     cnt <= cnt_next;
   end
 
-  assign cnt_next = i ? (cnt==3 ? cnt+1 : cnt) : 0;
+  assign cnt_next = I ? (cnt==3 ? cnt+1 : cnt) : 0;
   assign O = (cnt==3);
 
 endmodule
