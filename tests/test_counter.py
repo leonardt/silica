1b3a93d0f535e42487a1411c87fd40264e05c228
Lenny Truong <lenny@cs.stanford.edu>
2017-12-12 10:16:52 -0800

    Minor test update, update notebooks

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -16,16 +16,15 @@ def SilicaCounter(width, init=0, incr=1):
 
 def test_counter():
     N = 3
-    counter = SilicaCounter(3)
+    counter = SilicaCounter(N)
     for _ in range(2):
-        for i in range(1 << 3):
-            print(counter.O, counter.COUT)
+        for i in range(1 << N):
             next(counter)
             assert counter.O == i
-            assert counter.COUT == (i == (1 << 3) - 1)
+            assert counter.COUT == (i == (1 << N) - 1)
 
     magma_counter = silica.compile(counter, file_name="build/magma_counter.py")
-    check(magma_counter, SilicaCounter(3), 1<<3 * 2)
+    check(magma_counter, SilicaCounter(N), 1<<3 * 2)
     from mantle import DefineCounter
 
     check(DefineCounter(4), SilicaCounter(4), 1<<4 * 2)
