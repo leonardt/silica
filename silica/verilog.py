5520b400c95a4dc9dc93950991d9c795cdecc0d1
Lenny Truong <lenny@stanford.edu>
2018-09-12 13:16:26 -0700

    Add missing file

---
+++ silica/verilog.py
@@ -0,0 +1,35 @@
+import astor
+import ast
+
+
+class RemoveMagmaFuncs(ast.NodeTransformer):
+    def visit_Call(self, node):
+        if isinstance(node.func, ast.Name) and node.func.id in ['bits', 'uint']:
+            return node.args[0]
+        return node
+
+
+tab = "    "
+
+
+def compile_state(state, index, _tab, one_state):
+    offset = ""
+    verilog_source = ""
+    if state.conds:
+        offset = tab
+        cond = " & ".join(astor.to_source(RemoveMagmaFuncs().visit(cond)).rstrip() for cond in state.conds)
+        if not one_state:
+            cond += f" & (yield_state == {state.start_yield_id})"
+        if index == 0:
+            if_str = "if"
+        else:
+            if_str = "else if"
+        verilog_source += f"\n{_tab}{if_str} ({cond}) begin"
+    for statement in state.statements:
+        RemoveMagmaFuncs().visit(statement)
+        verilog_source += f"\n{_tab + offset}" + astor.to_source(statement).rstrip() + ";"
+    if not one_state:
+        verilog_source += f"\n{_tab + offset}yield_state = {state.end_yield_id};"
+    if state.conds:
+        verilog_source += f"\n{_tab}end"
+    return verilog_source
