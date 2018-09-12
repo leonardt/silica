9213248ec257ceaa82099105742e7a76e02021c8
Lenny Truong <lenny@stanford.edu>
2018-09-12 13:02:13 -0700

    Add fault version requirement

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from setuptools import setup
 
-# from pip.req import parse_requirements
-
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-# install_reqs = parse_requirements("requirements.txt", session=False)
-
-# reqs = [str(ir.req) for ir in install_reqs]
-
 setup(
     name='silica',
     version='0.1-alpha',
     description='',
     packages=["silica"],
-    # install_requires=reqs
-    )
+    install_requires=[
+        "fault=0.21"
+    ]
+)
