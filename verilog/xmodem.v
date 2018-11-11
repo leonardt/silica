a21fe276eb7d5843ea0d8e0665afdbf1a4687d51
Ross <rdaly525@stanford.edu>
2018-11-11 08:41:34 -0800

    finished xmodem send message mechanims

--- verilog/xmodem.v
+++ verilog/xmodem.v
@@ -1,13 +1,13 @@
 `define TIMEOUT 1<<20
-`define SOH = 8'h1
-`define EOT = 8'h4
-`define ACK = 8'h6
-`define NAK = 8'h15
-`define ASCIC = 8'h43
-`define MAX_TIME = 1<<16-1
-
+`define SOH 8'h1
+`define EOT 8'h4
+`define ACK 8'h6
+`define NAK 8'h15
+`define ASCIC 8'h43
+`define MAX_TIME (1<<16-1)
+/*
 //This module behaves as the xmodem sender.
-module xmodem_sender(
+module xmodem_host(
     input clk,
  
     //Interface to outside world.
@@ -90,9 +90,8 @@ module byte_rx(
     input rx,
 )
 
-
 endmodule
-
+*/
 
 //This will send a full message
 module message_tx(
@@ -104,20 +103,41 @@ module message_tx(
     output [6:0] byte_addr,
     input [7:0] data,
  
-    output tx,
+    output tx
 );
+
+
+
     localparam WAIT=0,
                SEND_SOH=1,
                SEND_DP1=2,
                SEND_DP2=3,
                SEND_DATA=4,
                SEND_CHECKSUM=5;
-    
-    reg [6:0] data_cnt = 'h0;
+
+    reg [3:0] CS = WAIT;
+    reg [3:0] NS;
+
+    reg [6:0] data_cnt = 7'h0;
+    reg [6:0] data_cnt_next;
     assign byte_addr = data_cnt;
+   
+    wire is_last;
+    assign is_last = (data_cnt == 7'h7f);
+
+    wire uart_send;
+    wire uart_ready;
     
+    reg [7:0] check_sum = 8'h0;
+    reg [7:0] check_sum_next;
+
     reg [7:0] uart_byte;
     always @(*) begin
+        data_cnt_next = 7'h0;
+        check_sum_next = 8'h0;
+        message_sent = 1'b0;
+        uart_send = 1'b0;
+        uart_byte = 8'h0;
         case(CS)
             WAIT : begin
                 NS = send_message ? SEND_SOH : WAIT;
@@ -138,31 +158,40 @@ module message_tx(
                 NS = uart_ready ? SEND_DATA : SEND_DP2;
                 uart_send = uart_ready;
                 uart_byte = data;
+                check_sum_next = data;
             end
             SEND_DATA : begin
+                NS = uart_ready & is_last ? SEND_CHECKSUM : SEND_DATA;
                 uart_send = uart_ready;
-                uart_byte = () ? 
+                uart_byte = uart_ready & is_last ? check_sum : data;
+                check_sum_next = uart_ready ? (data + check_sum) : check_sum;
+                data_cnt_next = uart_ready ? data_cnt + 1'b1 : data_cnt;
             end
             SEND_CHECKSUM : begin
+                NS = uart_ready ? WAIT : SEND_CHECKSUM;
+                message_sent = uart_ready;
+            end
+            default : begin
+                NS = 4'hx;
             end
         endcase
             
     end
+    
+    always @(posedge clk) begin
+        CS <= NS;
+        check_sum <= check_sum_next;
+        data_cnt <= data_cnt_next;
+    end
 
-    wire uart_send;
-    wire uart_ready;
     uart_tx uart_tx_inst (
-        .clk(), 
-        .data(), 
-        .valid(), 
+        .clk(clk), 
+        .data(uart_byte), 
+        .valid(uart_send), 
         .tx(tx), 
         .ready(uart_ready)
     );
-);
     
-    
-);
-
 endmodule
 
 module uart_tx(
@@ -175,7 +204,7 @@ module uart_tx(
     reg [7:0] message;
     reg [2:0] send_cnt = 3'h0; //Was not initialized. 
     reg [1:0] state = 2'h0;
-    always @(posedge CLK) begin
+    always @(posedge clk) begin
         case (state)
             2'h0: 
                 if (valid) begin
@@ -202,20 +231,12 @@ module uart_tx(
                     tx <= 1'b1; // end bit
                     state <= 2'h0;
                 end
-        endcase
-        /* $display("valid=%d", valid); */
-        /* $display("tx=%d", tx); */
-        /* $display("send_cnt=%d", send_cnt); */
-    end
-    /* assign ready = state == 0 & ~valid; */
-endmodule
+            default: begin
+                ready <= 1'bx;
+                tx <= 1'bx; // end bit
+                state <= 2'hx;
 
-//module xmodem_rx(
-//  input clk,
-//  
-//  //
-//  output
-//  rx
-//
-//
-//);
+            end
+        endcase
+    end
+endmodule
