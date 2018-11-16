c347a09b728201f8071d1dbd75fe4c26a2de5431
Lenny Truong <lenny@stanford.edu>
2018-11-16 08:35:23 -0800

    Remove whitespace in verilog for LoC comparison

--- verilog/counter.v
+++ verilog/counter.v
@@ -1,8 +1,6 @@
 module vcounter(input CLK, output [2:0] O);
 reg [2:0] O;
-
 always @(posedge CLK) begin
     O <= O + 1'b1;
 end
-
 endmodule
--- verilog/detect111.v
+++ verilog/detect111.v
@@ -4,16 +4,10 @@ module detect111(
   output O
 );
   reg [1:0] cnt = 2'h0;
-
   wire [1:0] cnt_next;
-
   always @(posedge CLK) begin
     cnt <= cnt_next;
   end
-
-  // mealey version
   assign cnt_next = I ? (cnt==2'h3 ? cnt : cnt+1'b1) : 2'h0;
   assign O = (cnt_next==2'h3);
-  /* assign O = (cnt==3); */
-
 endmodule
