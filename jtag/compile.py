353023d9d979ddcc1008f7d81e17d24cd5f169ed
Lenny Truong <lenny@stanford.edu>
2019-10-15 15:37:01 -0700

    Merge classes

--- jtag/compile.py
+++ jtag/compile.py
@@ -194,6 +194,34 @@ def get_io(cls):
     return inputs, outputs
 
 
+def update_other_class_references(body, other_class, attr):
+    class Transformer(ast.NodeTransformer):
+        def visit_Attribute(self, node):
+            if isinstance(node.value, ast.Name) and node.value.id == other_class and node.attr == attr:
+                return ast.Attribute(ast.Name("cls", ast.Load()), other_class +
+                                     "_" + attr, ast.Load())
+            return node
+    return [Transformer().visit(s) for s in body]
+
+
+def update_this_class_references(body, this_class):
+    class Transformer(ast.NodeTransformer):
+        def visit_Attribute(self, node):
+            if isinstance(node.value, ast.Name) and node.value.id == "cls":
+                node.attr = this_class + "_" + node.attr
+            return node
+    return [Transformer().visit(s) for s in body]
+
+
+def update_first_class_references(body, first_class):
+    class Transformer(ast.NodeTransformer):
+        def visit_Attribute(self, node):
+            if isinstance(node.value, ast.Name) and node.value.id == first_class:
+                node.value.id = "cls"
+            return node
+    return [Transformer().visit(s) for s in body]
+
+
 def merge_classes(tree):
     assert isinstance(tree, ast.Module)
     new_body = []
@@ -206,16 +234,33 @@ def merge_classes(tree):
             first_class = child
         else:
             assert get_io(first_class) == get_io(child)
+            to_append = []
             for s in child.body:
                 if isinstance(s, ast.FunctionDef):
+                    first_class.body = \
+                        update_other_class_references(first_class.body,
+                                                      child.name, s.name)
+                    s.body = \
+                        update_this_class_references(s.body, child.name)
+                    s.body = \
+                        update_first_class_references(s.body, first_class.name)
                     s.name = child.name + "_" + s.name
-                    first_class.body.append(s)
+                    to_append.append(s)
+            first_class.body += to_append
     tree.body = new_body
     return tree
 
 
 def is_yield(node):
     return isinstance(node, ast.Assign) and isinstance(node.value, ast.Yield)
+
+
+def is_yield_from(node):
+    return isinstance(node, ast.Expr) and isinstance(node.value, ast.YieldFrom)
+
+
+def has_yield_from(l):
+    return any(is_yield_from(i) for i in l)
 
 
 def collect_paths(block):
@@ -233,6 +278,76 @@ def collect_paths(block):
     return paths
 
 
+class FuncPaths:
+    def __init__(self):
+        self.entry_paths = []
+        self.other_paths = []
+
+
+def build_func_to_paths_map(cfg):
+    func_to_paths_map = {}
+    for k, v in cfg.functioncfgs.items():
+        assert k not in func_to_paths_map
+        func_to_paths_map[k] = FuncPaths()
+        for block in v:
+            if block == v.entryblock:
+                if not is_yield(block.statements[0]):
+                    for exit_ in block.exits:
+                        target = exit_.target
+                        func_to_paths_map[k].entry_paths.extend([
+                            [block] + s for s in collect_paths(target)
+                        ])
+                else:
+                    func_to_paths_map[k].entry_paths.append([block])
+
+            if is_yield(block.statements[0]):
+                for exit_ in block.exits:
+                    target = exit_.target
+                    func_to_paths_map[k].other_paths.extend([
+                        [block] + s for s in collect_paths(target)
+                    ])
+    return func_to_paths_map
+
+
+def join_yield_froms(func_to_paths_map):
+    print(list(func_to_paths_map.keys()))
+    new_func_to_paths_map = {}
+    for k, v in func_to_paths_map.items():
+        new_entry_paths = []
+        new_other_paths = []
+        for orig, new in zip((v.entry_paths, v.other_paths),
+                             (new_entry_paths, new_other_paths)):
+            for path in orig:
+                num_yield_froms = sum(has_yield_from(block.statements) for block in
+                                      path)
+                assert num_yield_froms <= 1
+                if num_yield_froms == 1:
+                    block = path[-1]
+                    assert has_yield_from(block.statements)
+                    assert sum(map(is_yield_from, block.statements)) == 1
+                    for node in block.statements:
+                        if is_yield_from(node):
+                            func = node.value.value
+                            print(astor.to_source(node.value.value))
+                            if isinstance(func.func, ast.Attribute) and \
+                                    isinstance(func.func.value, ast.Name) and \
+                                    func.func.value.id == "cls":
+                                new.extend([
+                                    path + entry_path for entry_path in
+                                    func_to_paths_map[func.func.attr].entry_paths
+                                ])
+                            else:
+                                raise Exception(func.func.value.id)
+                else:
+                    new.append(path)
+        new_func_to_paths_map[k] = FuncPaths()
+        new_func_to_paths_map[k].entry_paths = new_entry_paths
+        new_func_to_paths_map[k].other_paths = new_other_paths
+    for k, v in new_func_to_paths_map.items():
+        func_to_paths_map[k].entry_paths = v.entry_paths
+        func_to_paths_map[k].other_paths = v.other_paths
+
+
 def compile(file):
     name = file[-3:]
     with open(file, 'r') as src_file:
@@ -240,42 +355,25 @@ def compile(file):
         tree = ast.parse(src, mode='exec')
     tree = specialize_init_methods(tree)
     tree = convert_to_class_methods(tree)
-    # tree = merge_classes(tree)
+    tree = merge_classes(tree)
     print(astor.to_source(tree))
+    raise Exception()
     cfg = CFGBuilder().build(name, tree)
     # cfg.build_visual(name, 'pdf')
     num_yields = sum(count_yields(s) for s in cfg.entryblock.statements)
-    paths = {}
-    for k, v in cfg.functioncfgs.items():
-        paths[k] = ([], [])  # entry paths, other paths
-        for block in v:
-            if block == v.entryblock:
-                if not is_yield(block.statements[0]):
-                    for exit_ in block.exits:
-                        target = exit_.target
-                        paths[k][0].extend([
-                            [block] + s for s in collect_paths(target)
-                        ])
-                else:
-                    paths[k][0].append([block])
-
-            if is_yield(block.statements[0]):
-                for exit_ in block.exits:
-                    target = exit_.target
-                    paths[k][1].extend([
-                        [block] + s for s in collect_paths(target)
-                    ])
-    for k, v in paths.items():
+    func_to_paths_map = build_func_to_paths_map(cfg)
+    join_yield_froms(func_to_paths_map)
+    for k, v in func_to_paths_map.items():
         print(f"Function == {k}")
         print(f"Entry paths")
-        for path in v[0]:
+        for path in v.entry_paths:
             print("===== PATH START =====")
             for block in path:
                 print(block.get_source().rstrip())
             print("=====  PATH END  =====")
             print()
         print(f"Other paths")
-        for path in v[1]:
+        for path in v.other_paths:
             print("===== PATH START =====")
             for block in path:
                 print(block.get_source().rstrip())
