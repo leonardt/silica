6609343a05405b503ae81c315e6ae76c31d63313
Lenny Truong <lenny@stanford.edu>
2019-10-18 15:36:54 -0700

    Fix cfg bug

--- jtag/fsm.v
+++ jtag/fsm.v
@@ -17,88 +17,84 @@ always @(posedge CLK or posedge RESET) begin
 end
 always @(*) begin
      case (state)
-        15: if (tms == 0) begin
+        15: if ((tms == 0)) begin
             next_state = 12;
-        end else if ((tms != 0)) begin
+        end else begin
             next_state = 15;
         end
-        12: if (tms == 1) begin
+        12: if ((tms == 1)) begin
             next_state = 7;
-        end else if ((tms != 1)) begin
+        end else begin
             next_state = 12;
         end
-        7: if (tms == 1) begin
+        7: if ((tms == 1)) begin
             next_state = 4;
-        end else if ((tms != 1)) begin
+        end else begin
             next_state = 6;
         end
-        4: if (tms == 1) begin
+        4: if ((tms == 1)) begin
             next_state = 15;
-        end else if ((tms != 1)) begin
+        end else begin
             next_state = 14;
         end
-        6: if (tms == 1) begin
+        6: if ((tms == 1)) begin
             next_state = 1;
-        end else if ((tms != 1)) begin
+        end else begin
             next_state = 2;
         end
-        2: if (tms == 1) begin
+        2: if ((tms == 1)) begin
             next_state = 1;
-        end else if ((tms != 1)) begin
+        end else begin
             next_state = 2;
         end
-        1: if (tms == 0) begin
+        1: if ((tms == 0)) begin
             next_state = 3;
-        end else if (tms == 0) begin
-            next_state = 0;
-        end else if ((tms != 0)) begin
+        end else begin
             next_state = 5;
         end
-        5: if (tms == 1) begin
+        5: if ((tms == 1)) begin
             next_state = 7;
-        end else if ((tms != 1)) begin
+        end else begin
             next_state = 12;
         end
-        3: if (tms == 0) begin
+        3: if ((tms == 0)) begin
             next_state = 3;
-        end else if () begin
+        end else begin
             next_state = 0;
         end
-        0: if (tms == 0) begin
+        0: if ((tms == 0)) begin
             next_state = 2;
-        end else if ((tms != 0)) begin
+        end else begin
             next_state = 5;
         end
-        14: if (tms == 1) begin
+        14: if ((tms == 1)) begin
             next_state = 9;
-        end else if ((tms != 1)) begin
+        end else begin
             next_state = 10;
         end
-        10: if (tms == 1) begin
+        10: if ((tms == 1)) begin
             next_state = 9;
-        end else if ((tms != 1)) begin
+        end else begin
             next_state = 10;
         end
-        9: if (tms == 0) begin
+        9: if ((tms == 0)) begin
             next_state = 11;
-        end else if (tms == 0) begin
-            next_state = 8;
-        end else if ((tms != 0)) begin
+        end else begin
             next_state = 13;
         end
-        13: if (tms == 1) begin
+        13: if ((tms == 1)) begin
             next_state = 7;
-        end else if ((tms != 1)) begin
+        end else begin
             next_state = 12;
         end
-        11: if (tms == 0) begin
+        11: if ((tms == 0)) begin
             next_state = 11;
-        end else if () begin
+        end else begin
             next_state = 8;
         end
-        8: if (tms == 0) begin
+        8: if ((tms == 0)) begin
             next_state = 10;
-        end else if ((tms != 0)) begin
+        end else begin
             next_state = 13;
         end
     endcase
